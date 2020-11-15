7f4d0d4deb0c347de3416134ed0423daa79f65e6
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-11-15 18:25:24 +0000

    feature/status-bar & header setup/style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import { StatusBar } from 'react-native';
 import Routes from './routes';
 
 const App = () => {
   return (
     <>
+      <StatusBar barStyle="dark-content" backgroundColor="#f5f5f5" />
       <Routes />
     </>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Image } from 'react-native';
 
+import logo from './assets/instagram.png';
 import Feed from './screens/Feed';
 
 const Routes = () => {
@@ -10,7 +12,12 @@ const Routes = () => {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{
-        headerTitle: 'Route Name',
+        headerTitle: <Image source={logo} />,
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#f5f5f5',
+          height: '100%',
+        }
       }}>
         <Screen name="Feed" component={Feed} />
       </Navigator>  
